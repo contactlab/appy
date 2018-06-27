5f5c5842506cdde33fe5f07c8c7caa42f683487e
StefanoMagrassi <stefano.magrassi@gmail.com>
2018-06-27 12:23:27 +0200

    jest-fetch-mock type extends jest.MockInstance
    re #11

--- test/_helpers/mock-fetch.ts
+++ test/_helpers/mock-fetch.ts
@@ -3,11 +3,9 @@ import * as mockFetch from 'jest-fetch-mock';
 declare global {
   namespace NodeJS {
     interface Global {
-      fetch: JFM;
+      fetch: typeof mockFetch;
     }
   }
-  type JFM1 = typeof mockFetch;
-  export interface JFM extends JFM1, jest.MockInstance<any> {}
 }
 
 global.fetch = mockFetch;
