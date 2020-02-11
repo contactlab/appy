dc5670ee02c86e2c5e528fedf65d7f466994db45
StefanoMagrassi <1291271+StefanoMagrassi@users.noreply.github.com>
2020-02-11 16:55:13 +0100

    withDecoder combinator

---
+++ docs/modules/combinators/decoder.ts.md
@@ -0,0 +1,79 @@
+---
+title: combinators/decoder.ts
+nav_order: 2
+parent: Modules
+---
+
+# decoder overview
+
+`Decoder` combinator: decodes `Resp` of a `Req<A>` and returns a `Req<B>`.
+
+Added in v3.0.0
+
+---
+
+<h2 class="text-delta">Table of contents</h2>
+
+- [Decoder (interface)](#decoder-interface)
+- [GenericDecoder (interface)](#genericdecoder-interface)
+- [toDecoder](#todecoder)
+- [withDecoder](#withdecoder)
+
+---
+
+# Decoder (interface)
+
+A specialization of a `GenericDecoder` with `Left` type fixed to `Error`.
+
+**Signature**
+
+```ts
+export interface Decoder<A> extends GenericDecoder<Error, A> {}
+```
+
+Added in v3.0.0
+
+# GenericDecoder (interface)
+
+Encodes a generic decoder, namely a function which takes an `unknown` input (usually a JSON object) and tries to decodes it, returning a `Right<A>` if it succeeds or a `Left<E>` otherwise.
+
+**Signature**
+
+```ts
+export interface GenericDecoder<E, A> extends RE.ReaderEither<unknown, E, A> {}
+```
+
+Added in v3.0.0
+
+# toDecoder
+
+Converts a `GenericDecoder<E, A>` into a `Decoder<A>`.
+
+**Signature**
+
+```ts
+export function toDecoder<E, A>(
+  dec: GenericDecoder<E, A>,
+  onLeft: (e: E) => Error
+): Decoder<A> { ... }
+```
+
+Added in v3.0.0
+
+# withDecoder
+
+Applies `Decoder<B>` to the `Resp<A>` of a `Req` converting it to a `Resp<B>`.
+
+It returns a `Left<ResponseError>` in case the decoding fails.
+
+It automatically sets "JSON" request header's
+
+**Signature**
+
+```ts
+export function withDecoder<A, B>(
+  decoder: Decoder<B>
+): (req: Req<A>) => Req<B> { ... }
+```
+
+Added in v3.0.0
---
+++ test/decoder.spec.ts
@@ -0,0 +1,169 @@
+import fetchMock from 'fetch-mock';
+import {right, left} from 'fp-ts/lib/Either';
+import * as RTE from 'fp-ts/lib/ReaderTaskEither';
+import {pipe} from 'fp-ts/lib/pipeable';
+import * as t from 'io-ts';
+import {failure} from 'io-ts/lib/PathReporter';
+import {Decoder, toDecoder, withDecoder} from '../src/combinators/decoder';
+import * as appy from '../src/index';
+
+afterEach(() => {
+  fetchMock.reset();
+});
+
+test('withDecoder() should decodes `Resp` with provided decoder', async () => {
+  const response = new Response('{"id": 1234, "name": "foo bar"}');
+  fetchMock.mock('http://localhost/api/resources', response);
+
+  const request = withDecoder(decoderOK)(appy.get);
+
+  const result = await request('http://localhost/api/resources')();
+
+  expect(result).toEqual(
+    right({
+      response,
+      data: {id: 1234, name: 'foo bar'}
+    })
+  );
+});
+
+test('withDecoder() should decodes `Resp` with provided decoder - response data is empty string', async () => {
+  const response = new Response('');
+  fetchMock.mock('http://localhost/api/resources', response);
+
+  const request = withDecoder(decoderOK)(appy.get);
+
+  const result = await request('http://localhost/api/resources')();
+
+  expect(result).toEqual(
+    right({
+      response,
+      data: {}
+    })
+  );
+});
+
+test('withDecoder() should decodes `Resp` with provided decoder - response data is an object', async () => {
+  const response = new Response();
+  fetchMock.mock('http://localhost/api/resources', response);
+
+  const request = pipe(
+    appy.get,
+    RTE.map(resp => ({
+      response: resp.response,
+      data: {id: 5678, name: 'THIS IS BAAAZ!'}
+    })),
+    withDecoder(decoderOK)
+  );
+
+  const result = await request('http://localhost/api/resources')();
+
+  expect(result).toEqual(
+    right({
+      response,
+      data: {id: 5678, name: 'THIS IS BAAAZ!'}
+    })
+  );
+});
+
+test('withDecoder() should decodes `Resp` with provided decoder - response data stringified', async () => {
+  const response = new Response();
+  fetchMock.mock('http://localhost/api/resources', response);
+
+  const request = pipe(
+    appy.get,
+    RTE.map(resp => ({
+      response: resp.response,
+      data: 12345678
+    })),
+    withDecoder(decoderOK)
+  );
+
+  const result = await request('http://localhost/api/resources')();
+
+  expect(result).toEqual(
+    right({
+      response,
+      data: 12345678
+    })
+  );
+});
+
+test('withDecoder() should fail if response data cannot be parsed', async () => {
+  const response = new Response('{some: "bad", json: true}');
+  fetchMock.mock('http://localhost/api/resources', response);
+
+  const request = withDecoder(decoderOK)(appy.get);
+
+  const result = await request('http://localhost/api/resources')();
+
+  expect(result).toEqual(
+    left({
+      type: 'ResponseError',
+      error: new Error('Unexpected token s in JSON at position 1'),
+      response
+    })
+  );
+});
+
+test('withDecoder() should fail if decoding fails', async () => {
+  const response = new Response('{"id": 1234, "name": "foo bar"}');
+  fetchMock.mock('http://localhost/api/resources', response);
+
+  const request = withDecoder(decoderKO)(appy.get);
+
+  const result = await request('http://localhost/api/resources')();
+
+  expect(result).toEqual(
+    left({
+      type: 'ResponseError',
+      error: new Error('decoding failed'),
+      response
+    })
+  );
+});
+
+test('toDecoder() should convert a `GenericDecoder` into a `Decoder`', () => {
+  const d = toDecoder(
+    iotsPayload.decode,
+    e => new Error(failure(e).join('\n'))
+  );
+
+  expect(d({id: 1234, name: 'foo bar', active: true})).toEqual(
+    right({
+      id: 1234,
+      name: 'foo bar',
+      active: true
+    })
+  );
+
+  expect(d({id: false})).toEqual(
+    left(
+      new Error(
+        `Invalid value false supplied to : Payload/id: number
+Invalid value undefined supplied to : Payload/name: string`
+      )
+    )
+  );
+});
+
+// --- Helpers
+interface Payload {
+  id: number;
+  name: string;
+}
+
+const decoderOK: Decoder<Payload> = u => {
+  const p = u as Payload;
+  return right(p);
+};
+
+const decoderKO: Decoder<Payload> = _ => left(new Error('decoding failed'));
+
+const iotsPayload = t.type(
+  {
+    id: t.number,
+    name: t.string
+  },
+  'Payload'
+);
