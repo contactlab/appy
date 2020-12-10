44af62a381ae5c29f3c1f3dec46c841f99f17a74
StefanoMagrassi <1291271+StefanoMagrassi@users.noreply.github.com>
2020-12-10 14:39:31 +0100

    upgrade dependencies - minor

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+import type {Config} from '@jest/types';
+
+const config: Config.InitialOptions = {
+  automock: false,
+  bail: true,
+  collectCoverage: true,
+  coveragePathIgnorePatterns: [
+    '<rootDir>/test/_setup.ts',
+    '<rootDir>/node_modules/'
+  ],
+  coverageReporters: ['text'],
+  globals: {
+    'ts-jest': {
+      diagnostics: true
+    }
+  },
+  moduleFileExtensions: ['js', 'json', 'node', 'ts'],
+  preset: 'ts-jest',
+  roots: ['<rootDir>/test/'],
+  setupFiles: ['<rootDir>/test/_setup.ts'],
+  testMatch: undefined,
+  testRegex: '(\\.|/)spec\\.ts$'
+};
+
+export default config;
