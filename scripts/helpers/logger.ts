57a7eeb730c3e91612d1ce1a4d38d1ed017da6aa
StefanoMagrassi <1291271+StefanoMagrassi@users.noreply.github.com>
2023-03-07 15:25:17 +0100

    remove glob dependency

--- scripts/helpers/logger.ts
+++ scripts/helpers/logger.ts
@@ -3,13 +3,11 @@ import {rightIO} from 'fp-ts/TaskEither';
 import {Eff} from './program';
 
 export interface Logger {
-  readonly debug: (s: string) => Eff<void>;
   readonly info: (s: string) => Eff<void>;
   readonly log: (s: string) => Eff<void>;
 }
 
 export const loggerConsole: Logger = {
-  debug: s => rightIO(log(`> ${s}`)),
   info: s => rightIO(info(`> ${s}`)),
   log: s => rightIO(log(`> ${s}`))
 };
